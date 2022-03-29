aead7af334a2bdc9fa1da589b8b8e61a729a8033
Gaurav Gupta <35700631+gg1711@users.noreply.github.com>
2022-03-30 04:07:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import random, string
 import os
 from key_generation_peers import Peers
 from document_lifecycle import Document
+import time
+import matplotlib.pyplot as plt
 
 
 def get_text(len):
     return ''.join(random.choice(string.ascii_letters) for i in range(len))
 
 
+def generate_graph(x, y, xlabel, ylabel, title):
+    plt.plot(x,y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+
+
 if __name__ == '__main__':
 
-    groups = ['Computer Science', 'Electrical']
+    groups = ['G1', 'G2']
 
     group1 = Peers(groups[0], is_group = True)
     group2 = Peers(groups[1], is_group = True)
@@ -22,7 +32,7 @@ if __name__ == '__main__':
         group2.add_user()
     print(f"groups available are:  {group1.name} and {group2.name}")
 
-    users = ['gaurav', 'rajat', 'rahul', 'alice', 'bob', 'charlie', 'admin']
+    users = ['A','B','C', 'D','E','F']
     user_objects = dict()
 
     for user in users:
@@ -31,7 +41,7 @@ if __name__ == '__main__':
     print("Users available are: ", end="")
     print(users)
 
-    user_groups = [[group1], [group1], [group1], [group2], [group2], [group2], [group1, group2]]
+    user_groups = [[group1], [group1], [group1], [group2], [group1, group2], [group2]]
     arg = input('Want to generate new keys for Users YES/NO: ')
 
     if arg == 'YES':
@@ -50,34 +60,59 @@ if __name__ == '__main__':
     if arg == 'YES':
         doc.create_new_file()
 
-        doc.write_file(user_objects['gaurav'], group1, '17cs02005')
-        doc.write_file(user_objects['rahul'], group1, '17cs02003')
-        doc.write_file(user_objects['alice'], group2, get_text(15))
-        doc.write_file(user_objects['rajat'], group1, '17cs02007')
-        doc.write_file(user_objects['admin'], group1, get_text(15))
-        doc.write_file(user_objects['charlie'], group2, get_text(15))
-        doc.write_file(user_objects['gaurav'], group1, '17cs02005')
-        doc.write_file(user_objects['bob'], group2, get_text(15))
-        doc.write_file(user_objects['admin'], group2, 'I am admin')
+        doc.write_file(user_objects['A'], group1, '17cs02005')
+        doc.write_file(user_objects['B'], group1, '17cs02003')
+        doc.write_file(user_objects['D'], group2, get_text(15))
+        doc.write_file(user_objects['E'], group1, '17cs02007')
+        doc.write_file(user_objects['F'], group1, get_text(15))
+        doc.write_file(user_objects['D'], group2, get_text(15))
+        doc.write_file(user_objects['C'], group1, '17cs02005')
+        doc.write_file(user_objects['E'], group2, get_text(15))
+        doc.write_file(user_objects['F'], group2, 'I am admin')
 
         # doc.write_file(Peers('gaurav'), group2, '17cs02005')
 
     print(doc.num_lines())
-    doc.read_file(user_objects['admin'])
-    # doc.read_file(user_objects['gaurav'])
+    doc.read_file(user_objects['E'])
+    doc.read_file(user_objects['A'])
     doc.verify_digest()
 
-    actions = doc.get_lifecycle(user_objects['admin'], [group1])
+    actions = doc.get_lifecycle(user_objects['A'], [group1])
     print(f"\nactions performed in {group1.name} group")
     for action in actions:
         print(action)
 
-    actions = doc.get_lifecycle(user_objects['bob'], [group1])
+    actions = doc.get_lifecycle(user_objects['D'], [group1])
     print(f"\nactions performed in {group1.name} group")
     for action in actions:
         print(action)
 
-    actions = doc.get_lifecycle(user_objects['admin'], [group1, group2])
+    actions = doc.get_lifecycle(user_objects['E'], [group1, group2])
     print(f"\nactions performed in {group1.name} and {group2.name} group")
     for action in actions:
         print(action)
+
+
+#   This part is used to get graphs for writing and file size after performing operations
+
+#   generate graph of time taken vs write operations.
+#     doc.create_new_file()
+#     start = time.time()
+#     writes = []
+#     timer =  []
+#     filesize = []
+#     writes.append(0)
+#     filesize.append(0)
+#     timer.append(0)
+#     for i in range(1,21):
+#         for j in range(1000):
+#             doc.write_file(user_objects['gaurav'], group1, '17cs02005')
+#         timer.append(time.time()-start)
+#         writes.append(i*100)
+#         filesize.append(os.stat(filename).st_size)
+#
+#     print(timer)
+#     print(writes)
+#     print(filesize)
+#     generate_graph(writes, timer, 'num of writes', 'time (sec)', 'writes vs Time taken')
+#     generate_graph(writes, filesize, 'num of writes', 'filesize (Bytes)', 'writes vs filesize')
